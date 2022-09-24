da46d24e6307f4774129d059b510bcafd89f7f1e
Feul617 <ho0papa617@gmail.com>
2022-09-24 20:29:45 +0900

    실습 5번 완성

--- foundation 1-4 1-5/foundation 1-4 1-5/main.cpp
+++ foundation 1-4 1-5/foundation 1-4 1-5/main.cpp
@@ -1,4 +1,4 @@
-#define Pro 5
+#define Pro 4
 #include <iostream>
 #include <gl/glew.h> //--- 필요한 헤더파일 include
 #include <gl/freeglut.h>
@@ -390,7 +390,7 @@ void TimerFunction(int value)
 }
 
 #elif Pro == 5
-
+#include <cmath>
 //구조체
 
 typedef struct RECTAN {
@@ -398,25 +398,23 @@ typedef struct RECTAN {
 	GLfloat r, g, b;
 };
 
-RECTAN Rectan[6];
+RECTAN bg_rect[31];
+RECTAN main_rect;
 
 //랜덤인수설정
 std::random_device rd;
 std::mt19937 gen(rd());
-std::uniform_real_distribution<GLfloat> dis(0, 1);
+std::uniform_real_distribution<GLfloat> dis(-0.9, 0.9);
 
 //함수
 GLvoid drawScene(GLvoid);
 GLvoid Reshape(int w, int h);
 
-void Keyboard(unsigned char key, int x, int y);
 void Mouse(int button, int state, int x, int y);
 void Motion(int x, int y);
-void make_rect();
 
 //변수
-int shapecount = -1;
-int select_rect = 0;
+int shapecount = 29;
 bool mouse_button = false;
 GLfloat r = dis(gen), g = dis(gen), b = dis(gen);
 
@@ -438,7 +436,18 @@ void main(int argc, char** argv) { //--- 윈도우 출력하고 콜백함수 설정 { //--- �
 	else
 		std::cout << "GLEW Initialized\n";
 
-	glutKeyboardFunc(Keyboard);
+	for (int i = 1; i <= shapecount; i++) {
+		r = abs(dis(gen));
+		g = abs(dis(gen));
+		b = abs(dis(gen));
+
+		bg_rect[i].r = r; bg_rect[i].g = g; bg_rect[i].b = b;
+		bg_rect[i].first_x = dis(gen);
+		bg_rect[i].first_y = dis(gen);
+		bg_rect[i].second_x = bg_rect[i].first_x + 0.1f; bg_rect[i].second_y = bg_rect[i].first_y + 0.1f;
+
+	}
+
 	glutDisplayFunc(drawScene); // 출력 함수의 지정
 	glutReshapeFunc(Reshape); // 다시 그리기 함수 지정
 	glutMouseFunc(Mouse);
@@ -449,12 +458,19 @@ void main(int argc, char** argv) { //--- 윈도우 출력하고 콜백함수 설정 { //--- �
 GLvoid drawScene()//--- 콜백 함수: 그리기 콜백 함수 { glClearColor( 0.0f, 0.0f, 1.0f, 1.0f ); // 바탕색을 ‘blue’ 로 지정
 {
 	// 그리기 부분 구현: 그리기 관련 부분이 여기에 포함된다.
-	glClearColor(0.5f, 0.5f, 0.5f, 1.0);
+	glClearColor(1.f, 1.f, 1.f, 1.0);
 	glClear(GL_COLOR_BUFFER_BIT);
 
-	for (int i = 0; i <= shapecount; i++) {
-		glColor3f(Rectan[i].r, Rectan[i].g, Rectan[i].b);
-		glRectf(Rectan[i].first_x, Rectan[i].first_y, Rectan[i].second_x, Rectan[i].second_y);
+	if (mouse_button == true) {
+		main_rect.r = 0.f; main_rect.g = 0.f; main_rect.b = 0.f;
+		glColor3f(main_rect.r, main_rect.g, main_rect.b);
+		glRectf(main_rect.first_x, main_rect.first_y, main_rect.second_x, main_rect.second_y);
+	}
+
+	for (int i = 1; i <= shapecount; i++) {
+		glColor3f(bg_rect[i].r, bg_rect[i].g, bg_rect[i].b);
+		glRectf(bg_rect[i].first_x, bg_rect[i].first_y, bg_rect[i].second_x, bg_rect[i].second_y);
+		//std::cout << bg_rect[i].first_y << " " << std::endl;
 	}
 
 	glutSwapBuffers(); // 화면에 출력하기
@@ -465,44 +481,20 @@ GLvoid Reshape(int w, int h) //--- 콜백 함수: 다시 그리기 콜백 함수 {
 	glViewport(0, 0, w, h);
 }
 
-void Keyboard(unsigned char key, int x, int y)
-{
-	switch (key) {
-	case 'a':
-		if (shapecount < 4) {
-			shapecount++;
-			make_rect();
-			r = dis(gen);
-			g = dis(gen);
-			b = dis(gen);
-		}
-		else
-			std::cout << "사각형 개수 초과" << std::endl;
-		break;
-	}
-	glutPostRedisplay();
-}
-
 void Mouse(int button, int state, int x, int y)
 {
 	GLfloat real_x = (GLfloat)(x - (WIDHT / 2)) / (WIDHT / 2);
 	GLfloat real_y = (GLfloat)-(y - (HEIGHT / 2)) / (HEIGHT / 2);
 
 	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
-		for (int i = shapecount; i >= 0; i--) {
-			if (Rectan[i].first_x < real_x && Rectan[i].first_y < real_y && Rectan[i].second_x > real_x && Rectan[i].second_y > real_y) {
-				mouse_button = true;
-				select_rect = i;
-				std::cout << "선택된 사각형 : " << select_rect << std::endl;
-				break;
-			}
-		}
+		mouse_button = true;
+		std::cout << "true" << std::endl;
 	}
 
 	else if (state == GLUT_UP) {
 		mouse_button = false;
-		select_rect = -1;
 	}
+	glutPostRedisplay();
 }
 
 
@@ -512,26 +504,21 @@ void Motion(int x, int y)
 	GLfloat real_y = (GLfloat)-(y - (HEIGHT / 2)) / (HEIGHT / 2);
 
 	if (mouse_button == true) {
-		Rectan[select_rect].first_x = real_x - 0.2f;
-		Rectan[select_rect].first_y = real_y - 0.2f;
-		Rectan[select_rect].second_x = real_x + 0.2f;
-		Rectan[select_rect].second_y = real_y + 0.2f;
-		std::cout << Rectan[select_rect].first_x << " " << Rectan[select_rect].first_y << " " << Rectan[select_rect].second_x << std::endl;
+		main_rect.first_x = real_x - 0.1f;
+		main_rect.first_y = real_y - 0.1f;
+		main_rect.second_x = real_x + 0.1f;
+		main_rect.second_y = real_y + 0.1f;
+		for (int i = 1; i <= shapecount; i++) {
+			if(main_rect.first_x < bg_rect[i].second_x &&
+				main_rect.first_y < bg_rect[i].second_y && 
+				main_rect.second_x > bg_rect[i].first_x &&
+				main_rect.second_y > bg_rect[i].first_y){ 
+
+				bg_rect[i].first_x = bg_rect[i].first_y = bg_rect[i].second_x = bg_rect[i].second_y = 0;
+			}
+		}
 	}
 	glutPostRedisplay();
 }
-
-void make_rect()
-{
-	Rectan[shapecount].r = r; Rectan[shapecount].g = g; Rectan[shapecount].b = b;
-	Rectan[shapecount].first_x = -0.2f;
-	Rectan[shapecount].first_y = -0.2f;
-	Rectan[shapecount].second_x = 0.2f;
-	Rectan[shapecount].second_y = 0.2f;
-	std::cout << "사각형 생성" << std::endl;
-}
-
-
-
 
 #endif
