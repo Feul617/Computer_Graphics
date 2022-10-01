d3b33db49b3ab9b5327ee1ec833e0758dd9ad0f1
Feul617 <ho0papa617@gmail.com>
2022-10-02 02:45:01 +0900

    8번 9번 틀 생성

--- foundation 1-4 1-5/foundation 1-4 1-5/6~9.cpp
+++ foundation 1-4 1-5/foundation 1-4 1-5/6~9.cpp
@@ -1,4 +1,4 @@
-#define Pro 7
+#define Pro 9
 #include <iostream>
 #include <gl/glew.h> //--- 필요한 헤더파일 include
 #include<gl/freeglut.h>
@@ -349,6 +349,9 @@ void main(int argc, char** argv) { //--- 윈도우 출력하고 콜백함수 설정 { //--- �
 	else
 		std::cout << "GLEW Initialized\n";
 
+	for (int i = 0; i < 4; i++) {
+		Rectan[i].shpaetype = 3;
+	}
 
 	draw_triangle();
 
@@ -458,7 +461,6 @@ void InitBuffer()
 	//--- 1번째 VBO를 활성화하여 바인드하고, 버텍스 속성 (좌표값)을 저장
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
 
-	//--- 변수 diamond 에서 버텍스 데이터 값을 버퍼에 복사한다.
 	//--- triShape 배열의 사이즈: 9 * float
 	glBufferData(GL_ARRAY_BUFFER, 24 * sizeof(GLfloat), triShape, GL_STATIC_DRAW);
 
@@ -514,7 +516,6 @@ void draw_triangle()
 
 		Rectan[i].x1 = center_verx[i] - 0.1f * cos(theta[i]); Rectan[i].y1 = center_very[i] - 0.1f * sin(theta[i]);
 		Rectan[i].x2 = center_verx[i] + (0.1f * cos(theta[i]) - 0.6f * sin(theta[i])); Rectan[i].y2 = center_very[i] + (0.1f * sin(theta[i]) + 0.6f * cos(theta[i]));
-		Rectan[i].shpaetype = 0;
 
 		if (i < 3)
 			colors[i][i] = 1.f;
@@ -548,43 +549,591 @@ void Timer(int value)
 			if (Rectan[i].x1 < -1.f || Rectan[i].x2 < -1.f) {
 				move_x[i] *= -1;
 				theta[i] = 270 * PI / 180;
-				if (Rectan[i].shpaetype == 1) {
-					center_verx[i] -= 0.1f;
+				if (Rectan[i].shpaetype == 0) {
+					center_verx[i] -= 0.6f;
 				}
-				Rectan[i].shpaetype = 1;
+				Rectan[i].shpaetype = 2;
 			}
 
 			if (Rectan[i].x2 > 1.f || Rectan[i].x1 > 1.f) {
 				move_x[i] *= -1;
 				theta[i] = 90 * PI / 180;
-				if (Rectan[i].shpaetype == 1) {
-					center_verx[i] += 0.1f;
+				if (Rectan[i].shpaetype == 2) {
+					center_verx[i] += 0.6f;
 				}
-				Rectan[i].shpaetype = 1;
+				Rectan[i].shpaetype = 0;
 			}
 
 			if (Rectan[i].y1 < -1.f || Rectan[i].y2 < -1.f) {
 				move_y[i] *= -1;
 				theta[i] = 0;
-				if (Rectan[i].shpaetype == 0) {
-					center_very[i] += 0.2f;
+				if (Rectan[i].shpaetype == 1) {
+					center_very[i] -= 0.6f;
 				}
-				Rectan[i].shpaetype = 0;
+				Rectan[i].shpaetype = 3;
 			}
 
 			if (Rectan[i].y2 > 1.f || Rectan[i].y1 > 1.f) {
 				move_y[i] *= -1;
 				theta[i] = 180 * PI / 180;
-				if (Rectan[i].shpaetype == 0) {
-					center_very[i] += 0.2f;
+				if (Rectan[i].shpaetype == 3) {
+					center_very[i] += 0.6f;
 				}
-				Rectan[i].shpaetype = 0;
+				Rectan[i].shpaetype = 1;
 			}
 			center_verx[i] += move_x[i]; center_very[i] += move_y[i];
 		}
 	}
 	draw_triangle();
 	glBufferData(GL_ARRAY_BUFFER, 24 * sizeof(GLfloat), triShape, GL_STATIC_DRAW);
+	glutPostRedisplay(); // 화면 재 출력
+	if (!start)
+		return;
+	glutTimerFunc(100, Timer, 1);
+}
+
+#elif Pro == 8
+//구조체
+typedef struct RECTANGLE {
+	GLfloat x1, x2, y1, y2;
+	int shpaetype;
+};
+
+RECTANGLE Rectan[4];
+
+//변수
+GLchar* vertexsource, * fragmentsource;
+GLuint vertexshader, fragmentshader, vao, vbo[2];
+GLuint s_program, shaderID;
+GLfloat rColor = 1.f, bColor = 1.f, gColor = 1.f;
+GLint result;
+GLchar errorLog[512];
+
+GLfloat center_verx[4] = { -0.5f, 0.5f ,-0.5f ,0.5f }, center_very[4] = { 0.2f, 0.2f, -0.8f, -0.8f }; //삼각형 중심 좌표
+GLfloat interval_x = 0.2f, interval_y = 0.3f; //점 사이의 거리
+
+GLfloat move_x[4] = { 0.03f, -0.01f, 0.01f, -0.02f };
+GLfloat move_y[4] = { 0.01f, 0.02f, -0.005f, -0.01f };
+
+GLfloat triShape[4][3][2] = {}; //--- 삼각형 위치 값
+GLfloat colors[4][3] = {};
+GLfloat Linevertex[4][2] = {};
+
+bool start = false;
+float theta[4] = { 0, 0, 0, 0 };
+
+//함수
+GLvoid drawScene(GLvoid);
+GLvoid Reshape(int w, int h);
+
+void make_vertexShaders();
+void make_fragmentShaders();
+void InitBuffer();
+void InitShader();
+void draw_triangle();
+
+void Keyboard(unsigned char key, int x, int y);
+void Timer(int value);
+
+
+void main(int argc, char** argv) { //--- 윈도우 출력하고 콜백함수 설정 { //--- 윈도우 생성하기
+	glutInit(&argc, argv); // glut 초기화
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA); // 디스플레이 모드 설정
+	glutInitWindowPosition(100, 100); // 윈도우의 위치 지정
+	glutInitWindowSize(WIDTH, HEIGHT); // 윈도우의 크기 지정
+	glutCreateWindow("Example1"); // 윈도우 생성
+
+	//--- GLEW 초기화하기
+	glewExperimental = GL_TRUE;
+	if (glewInit() != GLEW_OK) // glew 초기화
+	{
+		std::cerr << "Unable to initialize GLEW" << std::endl;
+		exit(EXIT_FAILURE);
+	}
+
+	else
+		std::cout << "GLEW Initialized\n";
+
+	for (int i = 0; i < 4; i++) {
+		Rectan[i].shpaetype = 3;
+	}
+
+	draw_triangle();
+
+	InitShader();
+	InitBuffer();
+
+	glutDisplayFunc(drawScene); // 출력 함수의 지정
+	glutReshapeFunc(Reshape); // 다시 그리기 함수 지정
+	glutKeyboardFunc(Keyboard);
+	glutMainLoop(); // 이벤트 처리 시작 }
+}
+
+GLvoid drawScene()//--- 콜백 함수: 그리기 콜백 함수 { glClearColor( 0.0f, 0.0f, 1.0f, 1.0f ); // 바탕색을 ‘blue’ 로 지정
+{
+	int vColorLocation = glGetUniformLocation(s_program, "out_Color");
+	//--- 변경된 배경색 설정
+	glClearColor(rColor, gColor, bColor, 1.0f);
+	//glClearColor(1.0, 1.0, 1.0, 1.0f);
+
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	//--- 렌더링 파이프라인에 세이더 불러오기
+	glUseProgram(s_program);
+
+	//--- 사용할 VAO 불러오기
+	glBindVertexArray(vao);
+
+	glEnableVertexAttribArray(0);
+
+	//--- 삼각형 그리기
+	for (int i = 0; i < 4; i++) {
+		glUniform3f(vColorLocation, colors[i][0], colors[i][1], colors[i][2]);
+		glDrawArrays(GL_TRIANGLES, i * 3, 3);
+	}
+
+	glutSwapBuffers(); //--- 화면에 출력하기
+
+}
+
+GLvoid Reshape(int w, int h) //--- 콜백 함수: 다시 그리기 콜백 함수 {
+{
+	glViewport(0, 0, w, h);
+}
+
+void make_vertexShader()
+{
+	vertexsource = filetobuf("vertex.glsl");
+
+	//--- 버텍스 세이더 객체 만들기
+	vertexshader = glCreateShader(GL_VERTEX_SHADER);
+
+	//--- 세이더 코드를 세이더 객체에 넣기
+	glShaderSource(vertexshader, 1, (const GLchar**)&vertexsource, 0);
+
+	//--- 버텍스 세이더 컴파일하기
+	glCompileShader(vertexshader);
+
+	//--- 컴파일이 제대로 되지 않은 경우: 에러 체크
+
+	GLint result;
+	GLchar errorLog[512];
+
+	glGetShaderiv(vertexshader, GL_COMPILE_STATUS, &result);
+	if (!result)
+	{
+		glGetShaderInfoLog(vertexshader, 512, NULL, errorLog);
+		std::cerr << "ERROR: vertex shader 컴파일 실패\n" << errorLog << std::endl;
+		return;
+	}
+}
+
+void make_fragmentShader()
+{
+	fragmentsource = filetobuf("fragment.glsl");
+
+	//--- 프래그먼트 세이더 객체 만들기
+	fragmentshader = glCreateShader(GL_FRAGMENT_SHADER);
+
+	//--- 세이더 코드를 세이더 객체에 넣기
+	glShaderSource(fragmentshader, 1, (const GLchar**)&fragmentsource, 0);
+
+	//--- 프래그먼트 세이더 컴파일
+	glCompileShader(fragmentshader);
+
+	//--- 컴파일이 제대로 되지 않은 경우: 컴파일 에러 체크
+
+	GLint result;
+	GLchar errorLog[512];
+
+	glGetShaderiv(fragmentshader, GL_COMPILE_STATUS, &result);
+	if (!result)
+	{
+		glGetShaderInfoLog(fragmentshader, 512, NULL, errorLog);
+		std::cerr << "ERROR: fragment shader 컴파일 실패\n" << errorLog << std::endl;
+		return;
+	}
+}
+
+void InitBuffer()
+{
+	glGenVertexArrays(1, &vao); //--- VAO 를 지정하고 할당하기
+
+	glBindVertexArray(vao); //--- VAO를 바인드하기
+
+	glGenBuffers(2, vbo); //--- 2개의 VBO를 지정하고 할당하기
+
+	//--- 1번째 VBO를 활성화하여 바인드하고, 버텍스 속성 (좌표값)을 저장
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+
+	//--- triShape 배열의 사이즈: 9 * float
+	glBufferData(GL_ARRAY_BUFFER, 24 * sizeof(GLfloat), triShape, GL_STATIC_DRAW);
+
+	//--- 좌표값을 attribute 인덱스 0번에 명시한다: 버텍스 당 3* float
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0);
+
+	//--- attribute 인덱스 0번을 사용가능하게 함
+	glEnableVertexAttribArray(0);
+
+	////--- 2번째 VBO를 활성화 하여 바인드 하고, 버텍스 속성 (색상)을 저장
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
+
+	////--- 변수 colors에서 버텍스 색상을 복사한다.
+	////--- colors 배열의 사이즈: 9 *float 
+	glBufferData(GL_ARRAY_BUFFER, 8 * sizeof(GLfloat), Linevertex, GL_STATIC_DRAW);
+
+	////--- 색상값을 attribute 인덱스 1번에 명시한다: 버텍스 당 3*float
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, 0);
+
+	////--- attribute 인덱스 1번을 사용 가능하게 함.
+	glEnableVertexAttribArray(1);
+}
+
+void InitShader()
+{
+	make_vertexShader(); //--- 버텍스 세이더 만들기
+
+	make_fragmentShader(); //--- 프래그먼트 세이더 만들기
+
+	//-- shader Program
+	s_program = glCreateProgram();
+	glAttachShader(s_program, vertexshader);
+	glAttachShader(s_program, fragmentshader);
+	glLinkProgram(s_program);
+
+	//checkCompileErrors(s_program, "PROGRAM");
+	// 
+	//--- 세이더 삭제하기
+	glDeleteShader(vertexshader);
+	glDeleteShader(fragmentshader);
+
+	//--- Shader Program 사용하기
+	glUseProgram(s_program);
+}
+
+void draw_triangle()
+{
+	for (int i = 0; i < 4; i++) {
+
+		triShape[i][0][0] = center_verx[i] + (0.1f * cos(theta[i])); triShape[i][0][1] = center_very[i] + (0.1f * sin(theta[i]));   //오른쪽 점
+		triShape[i][1][0] = center_verx[i] - (0.1f * cos(theta[i])); triShape[i][1][1] = center_very[i] - (0.1f * sin(theta[i]));   //왼쪽 점
+		triShape[i][2][0] = center_verx[i] - (0.6f * sin(theta[i])); triShape[i][2][1] = center_very[i] + (0.6f * cos(theta[i]));	      //위쪽 점
+
+		Rectan[i].x1 = center_verx[i] - 0.1f * cos(theta[i]); Rectan[i].y1 = center_very[i] - 0.1f * sin(theta[i]);
+		Rectan[i].x2 = center_verx[i] + (0.1f * cos(theta[i]) - 0.6f * sin(theta[i])); Rectan[i].y2 = center_very[i] + (0.1f * sin(theta[i]) + 0.6f * cos(theta[i]));
+
+		if (i < 3)
+			colors[i][i] = 1.f;
+	}
+	std::cout << "\n" << std::endl;
+	std::cout << Rectan[0].x2 << ". " << Rectan[0].y2 << std::endl;
+	std::cout << Rectan[0].x1 << " " << Rectan[0].y1 << std::endl;
+	//std::cout << move_x[0] << std::endl;
+	colors[3][0] = 0.5f; colors[3][1] = 0.5f;
+}
+
+void Keyboard(unsigned char key, int x, int y)
+{
+	switch (key)
+	{
+	case 's':
+		start = !start;
+
+		glutTimerFunc(10, Timer, 1);
+		break;
+	}
+	glutPostRedisplay();
+}
+
+
+void Timer(int value)
+{
+	if (start == true) {
+		for (int i = 0; i < 4; i++) {
+
+			if (Rectan[i].x1 < -1.f || Rectan[i].x2 < -1.f) {
+				move_x[i] *= -1;
+				theta[i] = 270 * PI / 180;
+				if (Rectan[i].shpaetype == 0) {
+					center_verx[i] -= 0.6f;
+				}
+				Rectan[i].shpaetype = 2;
+			}
+
+			if (Rectan[i].x2 > 1.f || Rectan[i].x1 > 1.f) {
+				move_x[i] *= -1;
+				theta[i] = 90 * PI / 180;
+				if (Rectan[i].shpaetype == 2) {
+					center_verx[i] += 0.6f;
+				}
+				Rectan[i].shpaetype = 0;
+			}
+
+			if (Rectan[i].y1 < -1.f || Rectan[i].y2 < -1.f) {
+				move_y[i] *= -1;
+				theta[i] = 0;
+				if (Rectan[i].shpaetype == 1) {
+					center_very[i] -= 0.6f;
+				}
+				Rectan[i].shpaetype = 3;
+			}
+
+			if (Rectan[i].y2 > 1.f || Rectan[i].y1 > 1.f) {
+				move_y[i] *= -1;
+				theta[i] = 180 * PI / 180;
+				if (Rectan[i].shpaetype == 3) {
+					center_very[i] += 0.6f;
+				}
+				Rectan[i].shpaetype = 1;
+			}
+			center_verx[i] += move_x[i]; center_very[i] += move_y[i];
+		}
+	}
+	draw_triangle();
+	glBufferData(GL_ARRAY_BUFFER, 24 * sizeof(GLfloat), triShape, GL_STATIC_DRAW);
+	glutPostRedisplay(); // 화면 재 출력
+	if (!start)
+		return;
+	glutTimerFunc(100, Timer, 1);
+}
+
+#elif Pro == 9
+//구조체
+typedef struct RECTANGLE {
+	GLfloat x1, x2, y1, y2;
+	int shpaetype;
+};
+
+RECTANGLE Rectan[4];
+
+//변수
+GLchar* vertexsource, * fragmentsource;
+GLuint vertexshader, fragmentshader, vao, vbo;
+GLuint s_program, shaderID;
+GLfloat rColor = 1.f, bColor = 1.f, gColor = 1.f;
+GLint result;
+GLchar errorLog[512];
+
+GLfloat Circleshape[2] = {};
+int radius = 0;
+
+bool start = false;
+float theta[4] = { 0, 0, 0, 0 };
+
+//함수
+GLvoid drawScene(GLvoid);
+GLvoid Reshape(int w, int h);
+
+void make_vertexShaders();
+void make_fragmentShaders();
+void InitBuffer();
+void InitShader();
+
+void Keyboard(unsigned char key, int x, int y);
+void Timer(int value);
+
+
+void main(int argc, char** argv) { //--- 윈도우 출력하고 콜백함수 설정 { //--- 윈도우 생성하기
+	glutInit(&argc, argv); // glut 초기화
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA); // 디스플레이 모드 설정
+	glutInitWindowPosition(100, 100); // 윈도우의 위치 지정
+	glutInitWindowSize(WIDTH, HEIGHT); // 윈도우의 크기 지정
+	glutCreateWindow("Example1"); // 윈도우 생성
+
+	//--- GLEW 초기화하기
+	glewExperimental = GL_TRUE;
+	if (glewInit() != GLEW_OK) // glew 초기화
+	{
+		std::cerr << "Unable to initialize GLEW" << std::endl;
+		exit(EXIT_FAILURE);
+	}
+
+	else
+		std::cout << "GLEW Initialized\n";
+
+	for (int i = 0; i < 4; i++) {
+		Rectan[i].shpaetype = 3;
+	}
+
+	InitShader();
+	InitBuffer();
+
+	glutDisplayFunc(drawScene); // 출력 함수의 지정
+	glutReshapeFunc(Reshape); // 다시 그리기 함수 지정
+	glutKeyboardFunc(Keyboard);
+	glutMouseFunc(Mouse);
+	glutMainLoop(); // 이벤트 처리 시작 }
+}
+
+GLvoid drawScene()//--- 콜백 함수: 그리기 콜백 함수 { glClearColor( 0.0f, 0.0f, 1.0f, 1.0f ); // 바탕색을 ‘blue’ 로 지정
+{
+	int vColorLocation = glGetUniformLocation(s_program, "out_Color");
+	//--- 변경된 배경색 설정
+	glClearColor(rColor, gColor, bColor, 1.0f);
+	//glClearColor(1.0, 1.0, 1.0, 1.0f);
+
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	//--- 렌더링 파이프라인에 세이더 불러오기
+	glUseProgram(s_program);
+
+	//--- 사용할 VAO 불러오기
+	glBindVertexArray(vao);
+
+	glEnableVertexAttribArray(0);
+
+	//--- 삼각형 그리기
+
+	glutSwapBuffers(); //--- 화면에 출력하기
+
+}
+
+GLvoid Reshape(int w, int h) //--- 콜백 함수: 다시 그리기 콜백 함수 {
+{
+	glViewport(0, 0, w, h);
+}
+
+void make_vertexShader()
+{
+	vertexsource = filetobuf("vertex.glsl");
+
+	//--- 버텍스 세이더 객체 만들기
+	vertexshader = glCreateShader(GL_VERTEX_SHADER);
+
+	//--- 세이더 코드를 세이더 객체에 넣기
+	glShaderSource(vertexshader, 1, (const GLchar**)&vertexsource, 0);
+
+	//--- 버텍스 세이더 컴파일하기
+	glCompileShader(vertexshader);
+
+	//--- 컴파일이 제대로 되지 않은 경우: 에러 체크
+
+	GLint result;
+	GLchar errorLog[512];
+
+	glGetShaderiv(vertexshader, GL_COMPILE_STATUS, &result);
+	if (!result)
+	{
+		glGetShaderInfoLog(vertexshader, 512, NULL, errorLog);
+		std::cerr << "ERROR: vertex shader 컴파일 실패\n" << errorLog << std::endl;
+		return;
+	}
+}
+
+void make_fragmentShader()
+{
+	fragmentsource = filetobuf("fragment.glsl");
+
+	//--- 프래그먼트 세이더 객체 만들기
+	fragmentshader = glCreateShader(GL_FRAGMENT_SHADER);
+
+	//--- 세이더 코드를 세이더 객체에 넣기
+	glShaderSource(fragmentshader, 1, (const GLchar**)&fragmentsource, 0);
+
+	//--- 프래그먼트 세이더 컴파일
+	glCompileShader(fragmentshader);
+
+	//--- 컴파일이 제대로 되지 않은 경우: 컴파일 에러 체크
+
+	GLint result;
+	GLchar errorLog[512];
+
+	glGetShaderiv(fragmentshader, GL_COMPILE_STATUS, &result);
+	if (!result)
+	{
+		glGetShaderInfoLog(fragmentshader, 512, NULL, errorLog);
+		std::cerr << "ERROR: fragment shader 컴파일 실패\n" << errorLog << std::endl;
+		return;
+	}
+}
+
+void InitBuffer()
+{
+	glGenVertexArrays(1, &vao); //--- VAO 를 지정하고 할당하기
+
+	glBindVertexArray(vao); //--- VAO를 바인드하기
+
+	glGenBuffers(1, &vbo); //--- 2개의 VBO를 지정하고 할당하기
+
+	//--- 1번째 VBO를 활성화하여 바인드하고, 버텍스 속성 (좌표값)을 저장
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+
+	//--- triShape 배열의 사이즈: 9 * float
+	glBufferData(GL_ARRAY_BUFFER, 24 * sizeof(GLfloat), Circleshape, GL_STATIC_DRAW);
+
+	//--- 좌표값을 attribute 인덱스 0번에 명시한다: 버텍스 당 3* float
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0);
+
+	//--- attribute 인덱스 0번을 사용가능하게 함
+	glEnableVertexAttribArray(0);
+
+	////--- 2번째 VBO를 활성화 하여 바인드 하고, 버텍스 속성 (색상)을 저장
+	//glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
+
+	////--- 변수 colors에서 버텍스 색상을 복사한다.
+	////--- colors 배열의 사이즈: 9 *float 
+	//glBufferData(GL_ARRAY_BUFFER, 8 * sizeof(GLfloat), Linevertex, GL_STATIC_DRAW);
+
+	////--- 색상값을 attribute 인덱스 1번에 명시한다: 버텍스 당 3*float
+	//glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, 0);
+
+	////--- attribute 인덱스 1번을 사용 가능하게 함.
+	//glEnableVertexAttribArray(1);
+}
+
+void InitShader()
+{
+	make_vertexShader(); //--- 버텍스 세이더 만들기
+
+	make_fragmentShader(); //--- 프래그먼트 세이더 만들기
+
+	//-- shader Program
+	s_program = glCreateProgram();
+	glAttachShader(s_program, vertexshader);
+	glAttachShader(s_program, fragmentshader);
+	glLinkProgram(s_program);
+
+	//checkCompileErrors(s_program, "PROGRAM");
+	// 
+	//--- 세이더 삭제하기
+	glDeleteShader(vertexshader);
+	glDeleteShader(fragmentshader);
+
+	//--- Shader Program 사용하기
+	glUseProgram(s_program);
+}
+
+void Keyboard(unsigned char key, int x, int y)
+{
+	switch (key)
+	{
+	}
+	glutPostRedisplay();
+}
+
+void Draw_Cirlce(GLfloat x, GLfloat y)
+{
+	Circleshape[0] = x; Circleshape[1] = y;
+}
+
+void Mouse(int button, int state, int x, int y)
+{
+	GLfloat real_x = (GLfloat)(x - (WIDTH / 2)) / (WIDTH / 2);
+	GLfloat real_y = (GLfloat)-(y - (HEIGHT / 2)) / (HEIGHT / 2);
+
+	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
+		Draw_Cirlce(real_x, real_y);
+	}
+
+}
+
+void Timer(int value)
+{
+	if (start == true) {
+
+	}
+
+	glBufferData(GL_ARRAY_BUFFER, 24 * sizeof(GLfloat), Circleshape, GL_STATIC_DRAW);
 	glutPostRedisplay(); // 화면 재 출력
 	if (!start)
 		return;
