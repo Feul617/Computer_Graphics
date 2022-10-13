203536d915b30d079884bb05d709728d8217b96c
Feul617 <ho0papa617@gmail.com>
2022-10-13 19:58:30 +0900

    12번 생성

---
+++ Foundation 12~/Foundation 12~/12.cpp
@@ -0,0 +1,218 @@
+#include <iostream>
+#include <gl/glew.h> //--- 필요한 헤더파일 include
+#include<gl/freeglut.h>
+#include <gl/freeglut_ext.h>
+#include <gl/glm/glm.hpp>
+#include <gl/glm/ext.hpp>
+#include <gl/glm/gtc/matrix_transform.hpp>
+#include "file.h"
+#include <random>
+#include <windows.h>
+
+#define WIDTH 800
+#define HEIGHT 600
+
+//변수
+GLchar* vertexsource, * fragmentsource;
+GLuint vertexshader, fragmentshader, vao[3], vbo[3];
+GLuint s_program, shaderID;
+GLfloat rColor = 1.f, bColor = 1.f, gColor = 1.f;
+GLint result;
+GLchar errorLog[512];
+
+glm::vec3 middle_line[4] = {};
+
+//함수
+GLvoid drawScene(GLvoid);
+GLvoid Reshape(int w, int h);
+
+void make_vertexShader();
+void make_fragmentShader();
+void InitBuffer();
+void InitShader();
+
+//그리기 함수
+void draw_middle_line();
+
+void Keyboard(unsigned char key, int x, int y);
+
+
+void main(int argc, char** argv) { //--- 윈도우 출력하고 콜백함수 설정 //--- 윈도우 생성하기
+	glutInit(&argc, argv); // glut 초기화
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH); // 디스플레이 모드 설정
+	glutInitWindowPosition(100, 100); // 윈도우의 위치 지정
+	glutInitWindowSize(WIDTH, HEIGHT); // 윈도우의 크기 지정
+	glutCreateWindow("Example1"); // 윈도우 생성
+
+	glUseProgram(s_program);
+
+	//--- GLEW 초기화하기
+	glewExperimental = GL_TRUE;
+	if (glewInit() != GLEW_OK) // glew 초기화
+	{
+		std::cerr << "Unable to initialize GLEW" << std::endl;
+		exit(EXIT_FAILURE);
+	}
+
+	else
+		std::cout << "GLEW Initialized\n";
+
+	draw_middle_line();
+
+	InitShader();
+	InitBuffer();
+
+	glutDisplayFunc(drawScene); // 출력 함수의 지정
+	glutReshapeFunc(Reshape); // 다시 그리기 함수 지정
+	glutKeyboardFunc(Keyboard);
+	glutMainLoop(); // 이벤트 처리 시작 }
+}
+
+GLvoid drawScene()//--- 콜백 함수: 그리기 콜백 함수 { glClearColor( 0.0f, 0.0f, 1.0f, 1.0f ); // 바탕색을 ‘blue’ 로 지정
+{
+	int vColorLocation = glGetUniformLocation(s_program, "out_Color");
+	//--- 변경된 배경색 설정
+	glClearColor(rColor, gColor, bColor, 1.0f);
+	//glClearColor(1.0, 1.0, 1.0, 1.0f);
+
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	//--- 렌더링 파이프라인에 세이더 불러오기
+	glUseProgram(s_program);
+
+	//--- 사용할 VAO 불러오기
+	glBindVertexArray(vao[0]);
+	glEnableVertexAttribArray(0);
+
+	//중앙선
+	glUniform3f(vColorLocation, 0, 0, 0);
+	glDrawArrays(GL_POINTS, 0, 4);
+	for (int i = 0; i < 4; i++) {
+		//glDrawArrays(GL_LINES, 3, i * 3);
+	}
+
+	glutSwapBuffers(); //--- 화면에 출력하기
+
+}
+
+GLvoid Reshape(int w, int h) //--- 콜백 함수: 다시 그리기 콜백 함수 {
+{
+	glViewport(0, 0, w, h);
+}
+
+void make_vertexShader()
+{
+	vertexsource = filetobuf("vertex.glsl");
+
+	//--- 버텍스 세이더 객체 만들기
+	vertexshader = glCreateShader(GL_VERTEX_SHADER);
+
+	//--- 세이더 코드를 세이더 객체에 넣기
+	glShaderSource(vertexshader, 1, (const GLchar**)&vertexsource, 0);
+
+	//--- 버텍스 세이더 컴파일하기
+	glCompileShader(vertexshader);
+
+	//--- 컴파일이 제대로 되지 않은 경우: 에러 체크
+
+	GLint result;
+	GLchar errorLog[512];
+
+	glGetShaderiv(vertexshader, GL_COMPILE_STATUS, &result);
+	if (!result)
+	{
+		glGetShaderInfoLog(vertexshader, 512, NULL, errorLog);
+		std::cerr << "ERROR: vertex shader 컴파일 실패\n" << errorLog << std::endl;
+		return;
+	}
+}
+
+void make_fragmentShader()
+{
+	fragmentsource = filetobuf("fragment.glsl");
+
+	//--- 프래그먼트 세이더 객체 만들기
+	fragmentshader = glCreateShader(GL_FRAGMENT_SHADER);
+
+	//--- 세이더 코드를 세이더 객체에 넣기
+	glShaderSource(fragmentshader, 1, (const GLchar**)&fragmentsource, 0);
+
+	//--- 프래그먼트 세이더 컴파일
+	glCompileShader(fragmentshader);
+
+	//--- 컴파일이 제대로 되지 않은 경우: 컴파일 에러 체크
+
+	GLint result;
+	GLchar errorLog[512];
+
+	glGetShaderiv(fragmentshader, GL_COMPILE_STATUS, &result);
+	if (!result)
+	{
+		glGetShaderInfoLog(fragmentshader, 512, NULL, errorLog);
+		std::cerr << "ERROR: fragment shader 컴파일 실패\n" << errorLog << std::endl;
+		return;
+	}
+}
+
+void InitBuffer()
+{
+	glGenVertexArrays(3, vao); //--- VAO 를 지정하고 할당하기
+	glGenBuffers(3, vbo); //--- 2개의 VBO를 지정하고 할당하기
+
+	//--- attribute 인덱스 0번을 사용가능하게 함
+	glBindVertexArray(vao[0]);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+
+	glBufferData(GL_ARRAY_BUFFER, 4 * sizeof(glm::vec3), middle_line, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+	////--- attribute 인덱스 1번을 사용 가능하게 함.
+	glEnableVertexAttribArray(0);
+
+}
+
+void InitShader()
+{
+	make_vertexShader(); //--- 버텍스 세이더 만들기
+	make_fragmentShader(); //--- 프래그먼트 세이더 만들기
+
+	//-- shader Program
+	s_program = glCreateProgram();
+	glAttachShader(s_program, vertexshader);
+	glAttachShader(s_program, fragmentshader);
+	glLinkProgram(s_program);
+
+	//checkCompileErrors(s_program, "PROGRAM");
+	// 
+	//--- 세이더 삭제하기
+	glDeleteShader(vertexshader);
+	glDeleteShader(fragmentshader);
+
+	//--- Shader Program 사용하기
+	glUseProgram(s_program);
+}
+
+void Keyboard(unsigned char key, int x, int y)
+{
+	switch (key)
+	{
+	case '1':
+
+		break;
+
+
+	case 'q':
+		glutLeaveMainLoop();
+		break;
+	}
+	glutPostRedisplay();
+}
+
+void draw_middle_line()
+{
+	//가로선
+	middle_line[0] = {1.f, 0, 0};  //오른쪽 점
+	middle_line[1] = {-1.f, 0, 0 };  //왼쪽 점
+	middle_line[2] = { 0, 1.f, 0 };  //위쪽 점
+	middle_line[3] = { 0, -1.f, 0 };  //아래쪽 점
+}
