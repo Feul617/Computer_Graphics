1129512fdc870c0abdcfec904f46b908b8d14b1b
Feul617 <ho0papa617@gmail.com>
2022-11-02 01:47:47 +0900

    16번
    다시 뜯어봐야할듯

--- Foundation16~/Foundation16~/file.h
+++ Foundation16~/Foundation16~/file.h
@@ -5,19 +5,25 @@
 
 char* filetobuf(const char* file)
 {
-	FILE* fptr;
+	FILE* fp;
 	long length;
 	char* buf;
 
-	fptr = fopen(file, "rb"); // Open file for reading 
-	if (!fptr) // Return NULL on failure 
-		return NULL;
-	fseek(fptr, 0, SEEK_END); // Seek to the end of the file 
-	length = ftell(fptr); // Find out how many bytes into the file we are 
-	buf = (char*)malloc(length + 1); // Allocate a buffer for the entire length of the file and a null terminator 
-	fseek(fptr, 0, SEEK_SET); // Go back to the beginning of the file 
-	fread(buf, length, 1, fptr); // Read the contents of the file in to the buffer 
-	fclose(fptr); // Close the file 
-	buf[length] = 0; // Null terminator 
-	return buf; // Return the buffer
+	fp = fopen(file, "rb");
+
+	if (!fp)
+		assert(fp != nullptr);
+
+	fseek(fp, 0, SEEK_END);
+	length = ftell(fp);
+	buf = (char*)malloc(sizeof(char) * length + 1);
+	fseek(fp, 0, SEEK_SET);
+	if (buf != nullptr)
+		fread(buf, 1, length + 1, fp);
+
+	fclose(fp);
+	if (buf != nullptr)
+		buf[length] = 0;
+
+	return buf;
 }
